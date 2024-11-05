cfec385491b213da9f744b26c12d40740539f38b
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-11-05 23:41:11 +0200

    Add: GSAP Animations

--- script.js
+++ script.js
@@ -8,3 +8,82 @@ burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   burger.classList.toggle("active");
 });
+
+
+gsap.from("#companies", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#companies",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+gsap.from("#expertise", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#expertise",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+gsap.from("#commitments", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#commitments",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+gsap.from("#our-work", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#our-work",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+gsap.from("partner-us", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "partner-us",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+gsap.from("#elevate", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#elevate",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+gsap.from("#methodology", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#elevate",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
